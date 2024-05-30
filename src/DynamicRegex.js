386404034e27f02fe3dcd3f81f8cb799803fd526
JoaoPauloJavascript <168126384+JoaoPauloJavascript@users.noreply.github.com>
2024-05-29 22:20:30 -0300

    Add files via upload

--- src/DynamicRegex.js
+++ src/DynamicRegex.js
@@ -49,8 +49,11 @@ const DynamicRegex = () => {
 
   return (
     <div className="dynamic-regex">
-      <h1>Dinâmica - REGEX</h1>
-      <h2>Mult Functional Counters</h2>
+      <div>
+        <h1>REGEX</h1>
+        <h2>Mult Functional Counters</h2>
+        <p>Valor: {value}</p>
+      </div>
       <div className="buttons">
         <button onClick={() => handleMathOperation('+')}>+</button>
         <button onClick={() => handleMathOperation('-')}>-</button>
@@ -59,14 +62,20 @@ const DynamicRegex = () => {
         <button onClick={() => handleMathOperation('x²')}>x²</button>
         <button onClick={() => handleMathOperation('x³')}>x³</button>
         <button onClick={() => handleMathOperation('0')}>0</button>
+        
       </div>
-      <input type="text" value={text} onChange={handleTextChange} placeholder="Digite algo..." />
-      <p>Comprimento do Texto: {count.length}</p>
-      <p>Alfabetos: {count.alphabets}</p>
-      <p>Números: {count.numbers}</p>
-      <p>Símbolos: {count.symbols}</p>
-      <p>Espaços: {count.spaces}</p>
-      <p>Valor: {value}</p>
+      <h2>Caracter Counter</h2>
+      <div>
+        <input type="text" value={text} onChange={handleTextChange} placeholder="Digite algo..." />
+        <div className="counters">
+          <p>Comprimento do Texto: {count.length}</p>
+          <p>Alfabetos: {count.alphabets}</p>
+          <p>Números: {count.numbers}</p>
+          <p>Símbolos: {count.symbols}</p>
+          <p>Espaços: {count.spaces}</p>
+          
+        </div>
+      </div>
     </div>
   );
 };
